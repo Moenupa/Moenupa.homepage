d57c92494b992525f797484238dcc70d31b1a174
Moenupa <mineworldy@outlook.com>
2021-03-23 00:02:14 +0800

    Fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,14 +19,57 @@
   border-radius: .4rem;
   border: .15rem solid var(--light);
 }
+:root {
+  /*to change color theme, edit bootstrap scss */
+  --blue: #007bff;
+	--indigo: #6610f2;
+	--purple: #6f42c1;
+	--pink: #e83e8c;
+	--red: #dc3545;
+	--orange: #fd7e14;
+	--yellow: #ffc107;
+	--green: #28a745;
+	--teal: #20c997;
+	--cyan: #17a2b8;
+  
+	--white: #fff;
+	--gray: #6c757d;
+	--light: #f8f9fa;
+	--dark: #343a40;
 
+  --blue-rgb: 0,123,255;
+	--indigo-rgb: 102,16,242;
+	--purple-rgb: 111,66,193;
+	--pink-rgb: 232,62,140;
+	--red-rgb: 220,53,69;
+	--orange-rgb: 253,126,20;
+	--yellow-rgb: 255,193,7;
+	--green-rgb: 40,167,69;
+	--teal-rgb: 32,201,151;
+	--cyan-rgb: 23,162,184;
+
+	--white-rgb: 255,255,255;
+	--gray-rgb: 108,117,125;
+	--light-rgb: 248,249,250;
+	--dark-rgb: 52,58,64;
+  
+	--primary: var(--blue);
+	--secondary: var(--gray);
+	--success: var(--green);
+	--info: var(--cyan);
+	--warning: var(--yellow);
+	--danger: var(--red);
+}
 :root[data-theme="dark"] {
   --white: #000;
   --gray: #6c757d;
-  --gray-dark: #f8f9fa;
-  --secondary: #6c757d;
   --light: #343a40;
   --dark: #f8f9fa;
+
+  --white-rgb: 0,0,0;
+	--gray-rgb: 108,117,125;
+	--light-rgb: 52,58,64;
+	--dark-rgb: 248,249,250;
 }
 body {
   background-color: var(--white);
--- pages/index.vue
+++ pages/index.vue
@@ -9,32 +9,31 @@
             <!-- author link: https://www.pixiv.net/users/23223750 not yet authorized-->
           </div>
           <h1>Moenupa WANG</h1>
-          <ul>
-            <li><i class="mdi mdi-cellphone"></i>Phone: +852 5496 8569</li>
-            <li><i class="mdi mdi-github"></i>Github: <a href="https://github.com/Moenupa">Moenupa</a></li>
-            <li><i class="mdi mdi-email"></i>E-Mail: <a href="mailto:Moenupa@outlook.com">Moenupa@outlook.com</a></li>
-            <li><i class="mdi mdi-wechat"></i>Wechat: <a href="https://u.wechat.com/MJ28uzxd5_hdvmexQCcPyws">Moenupa</a></li>
-          </ul>
+          <h5>Undergraduate Student at HKPolyU</h5>
           <div>
-            <span>
-              <a href="https://github.com/Moenupa"><span class="mdi mdi-github mdi-24px"></span></a>
-            </span>
-            <span>
-              <a href="mailto:Moenupa@outlook.com"><span class="mdi mdi-email mdi-24px"></span></a>
-            </span>
-            <span>
-              <a href="https://u.wechat.com/MJ28uzxd5_hdvmexQCcPyws"><span class="mdi mdi-wechat mdi-24px"></span></a>
-            </span>
+            <span><a href="https://github.com/Moenupa" target="_blank"><span class="mdi mdi-github mdi-24px"></span></a></span>
+            <span><a href="mailto:Moenupa@gamil.com" target="_blank"><span class="mdi mdi-email mdi-24px"></span></a></span>
+            <span><a href="https://u.wechat.com/MJ28uzxd5_hdvmexQCcPyws" target="_blank"><span class="mdi mdi-wechat mdi-24px"></span></a></span>
+            <span><a href="https://twitter.com/Moenupa" target="_blank"><span class="mdi mdi-twitter mdi-24px"></span></a></span>
+            <span><a href="https://facebook.com/Moenupa" target="_blank"><span class="mdi mdi-facebook mdi-24px"></span></a></span>
+          </div>
+          <div>
+            <span><a href="https://music.163.com/#/user/home?id=392696583" target="_blank"><span class="mdi mdi-music-circle mdi-24px"></span></a></span>
+            <span><a href="https://steamcommunity.com/id/moenupa/" target="_blank"><span class="mdi mdi-steam mdi-24px"></span></a></span>
+            <span><a href="https://youtube.com/channel/UCbOOUmgSADEx9TK4gMQS85w" target="_blank"><span class="mdi mdi-youtube mdi-24px"></span></a></span>
+            <span><a href="https://m.bilibili.com/space/380763134" target="_blank"><span class="mdi mdi-video-box mdi-24px"></span></a></span>
           </div>
         </div>
         <div class="bg-light text-center p-3 interests">
           <h2>Interests</h2>
-          <ul>
-            <li><i class="mdi mdi-music-box"></i>Music</li>
-            <li><i class="mdi mdi-chart-box"></i>Machine Learning</li>
-            <li><i class="mdi mdi-gamepad-circle"></i>Gaming</li>
-            <li><i class="mdi mdi-web-box"></i>Front-End</li>
-          </ul>
+          <table class="table text-left m-0">
+            <tbody>
+              <tr><td><span class="mdi mdi-music-box"></span></td><td>Music</td></tr>
+              <tr><td><span class="mdi mdi-chart-box"></span></td><td>Machine Learning</td></tr>
+              <tr><td><span class="mdi mdi-gamepad-circle"></span></td><td>Gaming</td></tr>
+              <tr><td><span class="mdi mdi-web-box"></span></td><td>Front-End</td></tr>
+            </tbody>
+          </table>
         </div>
         <div class="bg-light text-center p-3 introduction" >
           <h2>Introduction</h2>
@@ -45,44 +44,23 @@
           <h2>Courses</h2>
           <table class="table text-left m-0">
             <tbody>
-              <tr>
-                <td><i class="mdi mdi-code-greater-than"></i></td>
-                <td>COMP1002 Computational Thinking</td>
-              </tr>
-              <tr>
-                <td><i class="mdi mdi-account-group"></i></td>
-                <td>COMP1901 Freshmen Seminar</td>
-              </tr>
-              <tr>
-                <td><i class="mdi mdi-currency-sign"></i></td>
-                <td>COMP1011 Programming Fundamentals</td>
-              </tr>
-              <tr>
-                <td><i class="mdi mdi-laptop"></i></td>
-                <td>COMP1411 Introduction to Computer Systems</td>
-              </tr>
-              <tr>
-                <td><i class="mdi mdi-data-matrix"></i></td>
-                <td>COMP1433 Introduction to Data Analysis</td>
-              </tr>
-              <tr>
-                <td><i class="mdi mdi-database"></i></td>
-                <td>COMP2411 Database Systems</td>
-              </tr>
-              <tr>
-                <td><i class="mdi mdi-code-not-equal-variant"></i></td>
-                <td>COMP3421 Web Application</td>
-              </tr>
+              <tr><td><span class="mdi mdi-code-greater-than"></span></td><td>COMP1002 Computational Thinking</td></tr>
+              <tr><td><span class="mdi mdi-account-group"></span></td><td>COMP1901 Freshmen Seminar</td></tr>
+              <tr><td><span class="mdi mdi-currency-sign"></span></td><td>COMP1011 Programming Fundamentals</td></tr>
+              <tr><td><span class="mdi mdi-laptop"></span></td><td>COMP1411 Introduction to Computer Systems</td></tr>
+              <tr><td><span class="mdi mdi-data-matrix"></span></td><td>COMP1433 Introduction to Data Analysis</td></tr>
+              <tr><td><span class="mdi mdi-database"></span></td><td>COMP2411 Database Systems</td></tr>
+              <tr><td><span class="mdi mdi-code-not-equal-variant"></span></td><td>COMP3421 Web Application</td></tr>
             </tbody>
           </table>
         </div>
         <div class="bg-light text-center py-3 gist-box github">
           <script src="https://gist.github.com/Moenupa/aab911df7d12ffabb0ff9e8cb5545597.js"></script>
         </div>
-        <div class="bg-light text-center py-3 gist-box steam">
+        <div class="bg-light text-center p-3 gist-box steam">
           <script src="https://gist.github.com/Moenupa/dd5e2c295036bcfa7251a8dfe5facabc.js"></script>
         </div>
-        <div class="bg-light text-center py-3 gist-box netease-music">
+        <div class="bg-light text-center p-3 gist-box netease-music">
           <script src="https://gist.github.com/Moenupa/30493d0c7ead93d676b4d6c8a29dbb8a.js"></script>
         </div>
       </div>
@@ -115,6 +93,7 @@
   grid-template-areas: "profile profile profile"   
                        "introduction introduction courses"
                        "interests interests courses"
+                       "github . courses"
                        "github steam steam"
                        "github netease-music netease-music"; 
 }
@@ -125,19 +104,12 @@
   border-radius: 10px;
 }
 
-ul {
-  justify-content: initial;
-  align-items: initial;
-  list-style: none;
-  text-align: initial;
-  padding: 0;
-  margin-bottom: 0;
-}
-li > .mdi {
-  margin-right: 5px;
-}
-table > tr:first-child > td, table > tr:first-child > td {
+table tr:first-child, table tr:first-child > td {
   border-top: none;
+}
+table, table tr, table th, table td {
+  color: inherit !important;
+  border-color: rgba(var(--dark-rgb), .5);
 }
 p {
   text-align: initial;
@@ -154,24 +126,13 @@ p:last-child {
 .steam { grid-area: steam }
 .netease-music { grid-area: netease-music }
 
-.gist-box .gist * {
+.gist-box * {
+  color: inherit !important;
   margin: 0 !important;
   border: none !important;
-  color: inherit !important;
   background-color: transparent !important;
 }
 </style>
 
 <script>
-export default {
-  data: () => ({
-
-  }),
-  mounted() {
-    document.getElementById("theme-switch").addEventListener("click", function(){console.log("YESSSSSSSSSSSS");});
-  },
-  methods: {
-
-  }
-}
 </script>
