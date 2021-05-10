02c84a94e458b10fff179cb9fb1016ebe08c783d
Moenupa <Moenupa@users.noreply.github.com>
2021-05-10 12:25:24 +0000

    deploy: 268af8b04d38f9af56cd6ad5ecfc648d56fef8e4

--- static/static/1620649454/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMoenupa.homepage\u002Fstatic\u002Fstatic\u002F1620649454",layout:"simple",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002FMoenupa.homepage\u002F",assetsPath:"\u002FMoenupa.homepage\u002Fstatic\u002F",cdnURL:a}}}}(null));
---
+++ static/static/1620649524/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMoenupa.homepage\u002Fstatic\u002Fstatic\u002F1620649524",layout:"simple",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002FMoenupa.homepage\u002F",assetsPath:"\u002FMoenupa.homepage\u002Fstatic\u002F",cdnURL:a}}}}(null));
