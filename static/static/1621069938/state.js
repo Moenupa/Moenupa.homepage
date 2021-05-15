744f483b23028b842ffd98bd34bfe40ad6d88b21
Moenupa <Moenupa@users.noreply.github.com>
2021-05-15 09:12:24 +0000

    deploy: 240d4ef21edd59f225271a4ef401e230aff62e53

---
+++ static/static/1621069938/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMoenupa.homepage\u002Fstatic\u002Fstatic\u002F1621069938",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FMoenupa.homepage\u002F",assetsPath:"\u002FMoenupa.homepage\u002Fstatic\u002F",cdnURL:a}}}}(null));
